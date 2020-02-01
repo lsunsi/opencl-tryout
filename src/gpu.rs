aaab9f6d9dc09f68990e741ed44de2d045e668ef
lsunsi <sunsi.lucas@gmail.com>
2020-02-01 14:07:22 -0300

    Add basic structure and simple equality test

---
+++ src/gpu.rs
@@ -0,0 +1,3 @@
+pub fn calculate(wealths: &[f32], (_ret, _vol): (f32, f32)) -> Vec<f32> {
+    wealths.to_vec()
+}
---
+++ tests/test.rs
@@ -0,0 +1,14 @@
+use opencl_tryout::{cpu, gpu};
+
+#[test]
+fn implementations_match() {
+    let wealths: Vec<_> = (1..100).map(|a| a as f32).collect();
+    let portfolio = (0.07, 0.11);
+
+    let result_cpu = cpu::calculate(&wealths, portfolio);
+    let result_gpu = gpu::calculate(&wealths, portfolio);
+
+    for (rc, rg) in result_cpu.into_iter().zip(result_gpu) {
+        assert!((rc - rg).abs() < 1e-6);
+    }
+}
