e92f4cb1459097da16f0373d558e9fd8818700ea
lsunsi <sunsi.lucas@gmail.com>
2020-02-01 14:39:30 -0300

    Add implementation for CPU calculation

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,13 @@
-pub fn calculate(wealths: &[f32], (_ret, _vol): (f32, f32)) -> Vec<f32> {
-    wealths.to_vec()
+pub fn calculate(wealths: &[f32], (ret, vol): (f32, f32)) -> Vec<f32> {
+    let mut result = vec![];
+
+    for prev_wealth in wealths {
+        for next_wealth in wealths {
+            let a = vol.recip() * ((next_wealth / prev_wealth).ln() - (ret - vol.powi(2) / 2.0));
+            let prob = (2.0 * std::f32::consts::PI).sqrt().recip() * (-a.powi(2) / 2.0).exp();
+            result.push(prob);
+        }
+    }
+
+    result
 }
